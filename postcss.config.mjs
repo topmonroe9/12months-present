8a2f3c3cb4bd3cf0d316a7fd381bf1b0e066475a
Egor Semenchenko <esemenchenko0@gmail.com>
2025-02-13 21:27:56 +0300

    Move safelist configuration from postcss.config.mjs to tailwind.config.mjs for better organization

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -3,16 +3,6 @@ const config = {
   plugins: {
     tailwindcss: {},
   },
-  safelist: [
-    {
-      pattern:
-        /^text-(5xl|2xl|3xl|lg|sm|gray-200|gray-300|gray-600|pink-300|pink-400)$/,
-    },
-    { pattern: /^font-(serif|semibold|medium)$/ },
-    { pattern: /^(italic|text-center|mt-2|w-full|object-cover|rounded-lg)$/ },
-    { pattern: /^(grid|grid-cols-1|grid-cols-2|grid-rows-2|gap-4)$/ },
-    { pattern: /^(shadow-lg|shadow-xl)$/ },
-  ],
 };
 
 export default config;
